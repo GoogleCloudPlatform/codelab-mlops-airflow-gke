dc4e6d7aefd92818ef7e8b07f418a536743f6f80
Merve Sayan <mervesayan@google.com>
2024-10-19 16:14:13 +0000

    enable resource manager api

--- terraform/modules/gcs/api.tf
+++ terraform/modules/gcs/api.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "resourcemanager" {
+  project            = var.project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "storage" {
   project            = var.project_id
   service            = "storage.googleapis.com"
