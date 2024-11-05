7d2a1b787d180c2c23d84c8a760b0cd787a0c8c2
Laurent Grangeau <lgrangeau@google.com>
2024-11-05 09:44:39 +0100

    feat: add dataset download

--- services/dataset-download/dataset.py
+++ services/dataset-download/dataset.py
@@ -2,17 +2,21 @@ import os
 import kagglehub
 from google.cloud import storage
 
-KAGGLE_USERNAME = "YOUR_KAGGLE_USERNAME_HERE"
-KAGGLE_KEY = "YOUR_KAGGLE_KEY_HERE"
-GCS_BUCKET = os.getenv("GCS_BUCKET", "mlops-codelab")
-
-storage_client = storage.Client(project="mlops-codelab")
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
+KAGGLE_KEY = os.getenv("KAGGLE_KEY")
+GCS_BUCKET = os.getenv("GCS_BUCKET")
 
 def upload_files(bucket_name, source_folder):
+    storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
-    for filename in os.listdir(source_folder):
-        blob = bucket.blob(filename)
-        blob.upload_from_filename(os.path.join(source_folder, filename))
+
+    for root, _, files in os.walk(source_folder):
+        for file in files:
+            local_file_path = os.path.join(root, file)
+            gcs_file_path = os.path.relpath(local_file_path, source_folder)
+            blob = bucket.blob(gcs_file_path)
+            blob.upload_from_filename(local_file_path)
+            print(f"Uploaded {local_file_path} to gs://{bucket_name}/{gcs_file_path}")
 
 # Download latest version
 path = kagglehub.dataset_download("priyamchoksi/rotten-tomato-movie-reviews-1-44m-rows")
