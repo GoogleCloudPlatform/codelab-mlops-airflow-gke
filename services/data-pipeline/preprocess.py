d1e020ce3a180d62e9146f78da06be07cf54d4b9
Laurent Grangeau <lgrangeau@google.com>
2024-11-05 11:01:28 +0100

    feat: refactor data ppipeline

--- services/data-pipeline/preprocess.py
+++ services/data-pipeline/preprocess.py
@@ -1,21 +1,23 @@
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import gcsfs
 import json
-import torch
-from transformers import AutoTokenizer
 from datasets import Dataset
 
 # Constants - if environment variables are not provided, take given defaults
-BUCKET_DATA_URL = os.getenv("BUCKET_DATA_URL", "gs://finetuning-data-bucket/rotten_tomatoes_movie_reviews.csv")
-PREPARED_DATA_URL = os.getenv("PREPARED_DATA_URL", "gs://finetuning-data-bucket/prepared_data.jsonl")
+PROJECT_ID = os.getenv("PROJECT_ID")
+BUCKET_DATA_NAME = os.getenv("BUCKET_DATA_NAME")
+
+DATASET_NAME = os.getenv("DATASET_NAME", "rotten_tomatoes_movie_reviews.csv")
+PREPARED_DATASET_NAME = os.getenv("PREPARED_DATA_URL", "prepared_data.jsonl")
 DATASET_LIMIT = int(os.getenv("DATASET_LIMIT", "100"))  # Process a limited number of rows, used 100 during testing phase but can be increased
 MODEL_ID = os.getenv("MODEL_ID", "google/gemma-2b")
-PROJECT_ID = os.getenv("PROJECT_ID", "mlops-airflow2")
+
+DATASET_URL = f"gs://{BUCKET_DATA_NAME}/{DATASET_NAME}"
+PREPARED_DATASET_URL = f"gs://{BUCKET_DATA_NAME}/{PREPARED_DATASET_NAME}"
 
 # Load the dataset
-print(f"Loading dataset from {BUCKET_DATA_URL}...")
+print(f"Loading dataset from {DATASET_URL}...")
 
 def transform(data):
     question = f"Review analysis for movie '{data['id']}'"
@@ -25,7 +27,7 @@ def transform(data):
     return {'text': template.format(question=question, context=context, answer=answer)}
 
 try:
-    df = pd.read_csv(BUCKET_DATA_URL, nrows=DATASET_LIMIT)
+    df = pd.read_csv(DATASET_URL, nrows=DATASET_LIMIT)
     print("Dataset loaded successfully.")
 
     # Drop rows with NaN values in relevant columns
@@ -42,11 +44,11 @@ try:
     print(dataset[0])
 
     # Save the prepared dataset to JSON lines format
-    with gcsfs.GCSFileSystem(project=PROJECT_ID).open(PREPARED_DATA_URL, 'w') as f:
+    with gcsfs.GCSFileSystem(project=PROJECT_ID).open(PREPARED_DATASET_URL, 'w') as f:
         for item in dataset:
             f.write(json.dumps(item) + "\n")
 
-    print(f"Prepared dataset saved to {PREPARED_DATA_URL}")
+    print(f"Prepared dataset saved to {PREPARED_DATASET_URL}")
     
 except Exception as e:
     print(f"Error during data loading or preprocessing: {e}")
