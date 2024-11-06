121be8451dedf04153920dee7d1a33245fbde41a
Laurent Grangeau <lgrangeau@google.com>
2024-11-06 11:07:12 +0100

    feat: e2e dag pipeline

--- ml-ops-dag.py
+++ ml-ops-dag.py
@@ -11,24 +11,30 @@ from kubernetes.client import models as k8s_models
 GCP_PROJECT_ID = Variable.get("GCP_PROJECT_ID")
 BUCKET_DATA_NAME = Variable.get("BUCKET_DATA_NAME")
 HF_TOKEN = Variable.get("HF_TOKEN")
+KAGGLE_USERNAME = Variable.get("KAGGLE_USERNAME")
+KAGGLE_KEY = Variable.get("KAGGLE_KEY")
 JOB_NAMESPACE = Variable.get("JOB_NAMESPACE", default_var="airflow")
 
 with DAG(dag_id="mlops-dag",
             start_date=datetime(2024,8,1),
             schedule_interval="@hourly",
             catchup=False) as dag:
-        
-        # Step 1: Create GCS Folder
-        create_folder = DummyOperator(
-            task_id="create_folder_task"
+
+        # Step 1: Fetch raw data to GCS Bucket
+        dataset_download = KubernetesPodOperator(
+            task_id="dataset_download_task",
+            namespace=JOB_NAMESPACE,
+            image="us-central1-docker.pkg.dev/mlops-codelab-440409/mlops-airflow-repo/dataset-download@sha256:b678e1a58f368a79960596638bcd148fde960d979f52803431b9cf54965fbfb7",
+            name="dataset-download",
+            service_account_name="airflow-mlops-sa",
+            env_vars={
+                    "KAGGLE_USERNAME":KAGGLE_USERNAME,
+                    "KAGGLE_KEY":KAGGLE_KEY,
+                    "GCS_BUCKET":BUCKET_DATA_NAME
+            }
         )
 
-        # Step 2: Fetch raw data to GCS Bucket
-        dataset_download = DummyOperator(
-            task_id="dataset_download_task"
-        )
-
-        # Step 3: Run GKEJob for data preparation
+        # Step 2: Run GKEJob for data preparation
         data_preparation = KubernetesPodOperator(
             task_id="data_pipeline_task",
             namespace=JOB_NAMESPACE,
@@ -43,7 +49,7 @@ with DAG(dag_id="mlops-dag",
             }
         )
 
-        # Step 4: Run GKEJob for fine tuning
+        # Step 3: Run GKEJob for fine tuning
         fine_tuning = KubernetesPodOperator(
             task_id="fine_tuning_task",
             namespace=JOB_NAMESPACE,
@@ -61,17 +67,17 @@ with DAG(dag_id="mlops-dag",
             }
         )
 
-        # Step 5: Run GKEJob for model serving
+        # Step 4: Run GKEJob for model serving
         model_evaluation = DummyOperator(
             task_id="model_evaluation_task"
         )
 
-        # Step 6: Run GKEJob for model serving
+        # Step 5: Run GKEJob for model serving
         model_serving = KubernetesPodOperator(
             task_id="model_serving",
             namespace=JOB_NAMESPACE,
-            image="us-central1-docker.pkg.dev/mlops-codelab-440409/mlops-airflow-repo/finetuning@sha256:52a1564cfc286942c427f95d7c03afb79826658c4f64babb7ee1ad8ffba60eb8",
-            name="fine-tuning",
+            image="us-central1-docker.pkg.dev/mlops-codelab-440409/mlops-airflow-repo/inference@sha256:e8d7a7cb897165f0ef484d2e6b7669df6b2c053bef3497327beb66a3b0da72f8",
+            name="model-serving",
             service_account_name="airflow-mlops-sa",
             startup_timeout_seconds=600,
             container_resources=k8s_models.V1ResourceRequirements(
@@ -85,4 +91,4 @@ with DAG(dag_id="mlops-dag",
             }
         )
 
-        create_folder >> dataset_download >> data_preparation >> fine_tuning >> model_evaluation >> model_serving
+        dataset_download >> data_preparation >> fine_tuning >> model_evaluation >> model_serving
